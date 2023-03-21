9f8b2da41f1dc1f0daed640b4abb21bdf64617a6
LeCodeMinister <ahmedalhindi@protonmail.com>
2023-03-21 13:24:39 +0000

    populating the repo

---
+++ src/main/java/group9/project/expensestrackingapp/ExpensesTrackingAppApplication.java
@@ -0,0 +1,13 @@
+package group9.project.expensestrackingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpensesTrackingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpensesTrackingAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/group9/project/expensestrackingapp/ExpensesTrackingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package group9.project.expensestrackingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpensesTrackingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
