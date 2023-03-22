92167788f0cbefe7ebb34e71bd5170fd2b0a573e
YBobat <BobatY8@gmail.com>
2023-03-22 17:21:31 +0000

    users getters and setters

--- src/main/java/User.java
+++ src/main/java/User.java
@@ -16,5 +16,49 @@ public class User {
         @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
         private List<Expense> expenses = new ArrayList<>();
 
+        public user() {}
+
+        public user(Long id, String name, String email, List<Expense> expenses) {
+            this.id = id;
+            this.name = name;
+            this.email = email;
+            this.expenses = expenses;
         }
-    }
+
+        public Long getId() {
+            return id;
+        }
+
+        public void setId(Long id) {
+            this.id = id;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getEmail() {
+            return email;
+        }
+
+        public void setEmail(String email) {
+            this.email = email;
+        }
+
+        public List<Expense> getExpenses() {
+            return expenses;
+        }
+
+        public void setExpenses(List<Expense> expenses) {
+            this.expenses = expenses;
+        }
+
+        @Override
+        public String toString() {
+            return "User [id=" + id + ", name=" + name + ", email=" + email + ", expenses=" + expenses + "]";
+        }
+    }}
