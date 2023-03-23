327b89ff65055d3fb247964f5abd68c72d55a0b1
YBobat <BobatY8@gmail.com>
2023-03-23 13:49:23 +0000

    started expenses

--- src/main/java/Expense.java
+++ src/main/java/Expense.java
@@ -1,2 +1,27 @@
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "expenses")
 public class Expense {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String description;
+
+    private double amount;
+
+    private String category;
+
+    private boolean isRepaid;
+
+    private Long date;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    }
+
 }
