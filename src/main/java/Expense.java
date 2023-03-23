8728c1d66af50c09d032e345688d98bc62ef90df
YBobat <BobatY8@gmail.com>
2023-03-23 14:12:58 +0000

    expenses getters n setters

--- src/main/java/Expense.java
+++ src/main/java/Expense.java
@@ -22,6 +22,73 @@ public class Expense {
     @JoinColumn(name = "user_id")
     private User user;
 
+
+    public Expense() {
+    }
+
+    public Expense(String description, double amount, String category, boolean isRepaid, Long date, User user) {
+        this.description = description;
+        this.amount = amount;
+        this.category = category;
+        this.isRepaid = isRepaid;
+        this.date = date;
+        this.user = user;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public boolean getIsRepaid() {
+        return isRepaid;
+    }
+
+    public void setIsRepaid(boolean isRepaid) {
+        this.isRepaid = isRepaid;
+    }
+
+    public Long getDate() {
+        return date;
+    }
+
+    public void setDate(Long date) {
+        this.date = date;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
     }
 
 }
